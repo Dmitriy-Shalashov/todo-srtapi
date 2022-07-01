2df486035b452e780e92519c5b6a726e8c3fef11
Dmitriy-Shalashov <dmitriy.tackle@gmail.com>
2022-07-02 00:01:06 +0300

    update: Detail add header form create new post

--- client/src/pages/detail/Detail.jsx
+++ client/src/pages/detail/Detail.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { saveListTitle } from "../../store/todoListsSlice";
-import { getAllPostsForList, cleareState } from "../../store/todoPostsSlice";
+import { getAllPostsForList } from "../../store/todoPostsSlice";
 import { useParams, useNavigate } from "react-router-dom";
 
 import Button from "../../common/button/Button";
 import PostItem from "../../common/post-item/PostItem";
 import Loader from "../../common/loader/Loader";
+import CreateNewPostForm from "../../common/create-post/CreateNewPostForm";
 
 import "./detail.scss";
 
@@ -14,18 +15,15 @@ const Detail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const navigate = useNavigate();
-  const { title, posts } = useSelector((state) => state.todoPosts.todoPosts);
-  const { status } = useSelector((state) => state.todoPosts);
-  console.log(title);
+  const { posts, title } = useSelector((state) => state.todoPosts.todoPosts);
   console.log(posts);
+  const { status } = useSelector((state) => state.todoPosts);
+
   useEffect(() => {
     dispatch(saveListTitle(id));
     dispatch(getAllPostsForList(id));
 
     window.scrollTo({ top: 0, behavior: "smooth" });
-    return () => {
-      dispatch(cleareState());
-    };
   }, [dispatch, id]);
 
   const goBack = () => {
@@ -36,11 +34,20 @@ const Detail = () => {
     <>
       <div className="container ">
         <div className="detail mt-6">
-          <div className="detail__btn">
-            <Button type="goBack" onClick={goBack} />
+          <div className="detail__header">
+            <div className="header__form">
+              <div className="form__wrap">
+                <CreateNewPostForm listId={id} />
+              </div>
+            </div>
+            <div className="header__btn">
+              <Button type="goBack" onClick={goBack} />
+            </div>
           </div>
           {status === "loading" && <Loader />}
-          {/* {!posts.length > 0 && <h2>List empty</h2>} */}
+          {!posts.length > 0 && (
+            <h2>List "{title}" is empty, pleace create new posts</h2>
+          )}
           <div className="detail__wrapper ">
             {posts.map((post) => (
               <PostItem
